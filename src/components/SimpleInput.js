1cdbee5539337f4bfeef9688cf7815009a88f3c8
Yeliazar <erik.ruzhakov@mail.ru>
2022-09-30 00:39:48 +0300

    Add Validation text

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,9 +2,9 @@ import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
 
-    const nameInputRef = useRef();
-
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = event => {
       setEnteredName(event.target.value);
@@ -14,8 +14,11 @@ const SimpleInput = (props) => {
         event.preventDefault();
 
       if(enteredName.trim() === '') {
+          setEnteredNameIsValid(false);
           return;
       }
+
+      setEnteredNameIsValid(true);
 
       const enteredValue = nameInputRef.current.value;
       console.log(enteredValue);
@@ -23,9 +26,11 @@ const SimpleInput = (props) => {
       setEnteredName('');
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
             ref={nameInputRef}
@@ -34,6 +39,7 @@ const SimpleInput = (props) => {
             onChange={nameInputChangeHandler}
             value={enteredName}
         />
+          {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
