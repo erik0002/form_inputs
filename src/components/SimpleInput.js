7425e37dd8141957166b98b396a41610611c7396
Yeliazar <erik.ruzhakov@mail.ru>
2022-09-30 01:12:59 +0300

    Refactoring code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,11 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputsIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = event => {
       setEnteredName(event.target.value);
@@ -13,12 +13,6 @@ const SimpleInput = (props) => {
 
   const nameInputBlurHandler = event => {
       setEnteredNameTouched(true);
-
-      if ( enteredName.trim() === '') {
-          setEnteredNameIsValid(false);
-          return;
-      }
-
   };
 
   const formSubmissionHandler = event => {
@@ -26,20 +20,14 @@ const SimpleInput = (props) => {
 
       setEnteredNameTouched(true);
 
-      if(enteredName.trim() === '') {
-          setEnteredNameIsValid(false);
+      if(!enteredNameIsValid) {
           return;
       }
 
-      setEnteredNameIsValid(true);
-
-      const enteredValue = nameInputRef.current.value;
-      console.log(enteredValue);
       //nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
       setEnteredName('');
+      setEnteredNameTouched(false);
   };
-
-  const nameInputsIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputsIsInvalid ? 'form-control invalid' : 'form-control';
 
@@ -48,7 +36,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-            ref={nameInputRef}
             type='text'
             id='name'
             onChange={nameInputChangeHandler}
